a99bdf20f4056007753d14edf6f61f7350190742
KangDayoung <kallzero1008@jbnu.ac.kr>
2022-11-17 21:16:15 +0900

    Blog category 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,46 @@
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
+from django.shortcuts import render
 
 
 # template_name 설정 안하면 post_list.html 로 자동 설정
 class PostList(ListView):
     model = Post
     ordering = "-pk"
-    # template_name = 'blog.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+
+        return context
+
+def category_page(request, slug):
+    # category = Category.objects.get(slug=slug)
+    if slug == 'no_category':
+        category = '미분류',
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
 
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+
+        return context
